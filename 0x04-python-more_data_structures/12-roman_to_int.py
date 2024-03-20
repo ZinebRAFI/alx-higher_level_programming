c11238f471448966297050acbcabaeaefe80d3fa
Your Name <you@example.com>
2024-03-20 04:14:23 -0700

    up

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    sum = 0
-    index = 0
-    if roman_string is not None:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
-        listrom = list(roman.keys())
-        length = len(roman_string)
-        for char in roman_string:
-            # print("++ "+char)
-            if char in roman:
-                # print("+ "+char, roman[char])
-                if char in 'IXC':
-                    if index < length-1:
-                        if listrom[index+1] in 'VXLCDM':
-                            sum = sum + (roman[listrom[index+1]]-roman[char])
-                sum += roman[char]
-            index += 1
-    return sum
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
 
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
+        else:
+            rep -= numbers[i]
 
-"""
-check if current charachter index is equal to lenght
-"I" can be subtracted from "V" and "X",
-"X" can be subtracted from "L" and "C",
-"C" can be subtracted from "D" and "M".
-However, "V", "L", and "D" cannot be used to represent subtraction.
-"""
+    return rep
