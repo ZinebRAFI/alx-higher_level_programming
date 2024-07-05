582c8a7c938f35c1259b8dd0c3e81c3a492f6c1a
Your Name <you@example.com>
2024-07-05 04:28:49 -0700

    up

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" module doc """
+"""Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        utf_data = data.decode('utf-8')
-        resType = type(data)
-        print(f"Body response:\n\t- type: {resType}\n\t\
-- content: {data}\n\t- utf8 content: {utf_data}")
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
